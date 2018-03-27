e471e4f7caddeae69349ed54ef3d5f4927ce9d3f
ryangrose <ryanwgrose@gmail.com>
2018-03-26 21:25:51 -0700

    Added file reading methods

--- check.hs
+++ check.hs
@@ -1,5 +1,6 @@
 import System.Environment (getArgs)
 import Data.Time.LocalTime
+import Data.List
 
 interactWith f inputFile outputFile = do
     input <- readFile inputFile
@@ -15,9 +16,30 @@ interactWith f inputFile outputFile = do
         {- -- Replace id with actual function -}
         {- myFunction = id -}
 
-printCurrentTime = do
+appendCurrentTime = do
     currTime <- getZonedTime
     let time = takeWhile (/= '.') $ show currTime
     appendFile "out.txt" (time ++ "\n")
 
-main = printCurrentTime
+divideByDays [] = []
+divideByDays (x:y:xs)
+    | (day x) == (day y) = [x,y]: divideByDays xs
+    | otherwise          = [x]: divideByDays (y:xs)
+
+divideByDays (x:[]) = [[x]]
+
+day d = takeWhile (/= ' ') d
+time d = drop 1 $ dropWhile (/= ' ') d
+
+showDayTimes dayTimes =
+    d ++ ": " ++ (concat $ intersperse " " $ map time dayTimes)
+    where d = day $ head dayTimes
+
+printDays = do
+    input <- readFile "out.txt"
+    let inputLines = lines input
+    let days = divideByDays inputLines
+    let prettyDays = map showDayTimes days
+    putStr $ unlines prettyDays
+
+main = printDays
